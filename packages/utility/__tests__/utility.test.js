799464a59ee59dc228a650162c93c74b242633b3
sunyawei006 <sunyawei006@ke.com>
2025-09-15 11:24:34 +0800

    初始化

---
+++ packages/utility/__tests__/utility.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const utility = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(utility(), 'Hello from utility');
+console.info('utility tests passed');
